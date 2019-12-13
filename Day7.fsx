2af5feedf657cfe20b6a8de6191da9a971fd3ddc
Tom Deryckere <Tom.Deryckere@gmail.com>
2019-12-13 10:38:16 -0500

    Adding part of day 7

--- Day7.fsx
+++ Day7.fsx
@@ -1,10 +1,18 @@
 open System.IO
 open System.Collections.Generic
 
-type Amplifier = (int * int) -> int
+// utility for creating permutations
+// Cheating I just copy pasted this stuff from stack overflow
+let rec distribute e = function
+  | [] -> [[e]]
+  | x::xs' as xs -> (e::xs)::[for xs in distribute e xs' -> x::xs]
 
+let rec permute = function
+  | [] -> [[]]
+  | e::xs -> List.collect (distribute e) (permute xs)
 
-// This one shouts for mutability
+///
+/// Let's define some types up front
 type ParameterMode = 
     | PositionMode
     | ImmediateMode
@@ -16,19 +24,23 @@ type Instruction = {
     Mode3: ParameterMode
     }
 
+type Amplifier = {
+    phase: int
+    programState: int []
+    initialized: bool
+    pointer: int
+    completed: bool
+    output: int
+}
+
+/// Get the instruction details for an opCode input
 let getInstruction (input: int): Instruction = 
     let modeA =  if (int (floor (float input / float 10000)) = 1) then ImmediateMode else PositionMode
     let rest1 = input % 10000
     let modeB =  if (int (floor (float rest1 / float 1000)) = 1 ) then ImmediateMode else PositionMode
     let rest2 = rest1 % 1000
     let modeC =  if (int (floor (float rest2 / float 100)) = 1 ) then ImmediateMode else PositionMode
-    // printfn "rest1 %i" rest1
-    // printfn "1st mode %A" modeC
-    // printfn "rest2 %i" rest2
-
     let instruction = rest1 % 100
-
-
     {
         Opcode=instruction;
         Mode1=modeC;
@@ -36,110 +48,208 @@ let getInstruction (input: int): Instruction =
         Mode3=modeA
     }
 
+/// get a value specified by an instruction. Take care of PositionMode vs ImmediateMode
 let getValue (mode: ParameterMode) (programState: int []) pointer =
     match mode with
         | PositionMode -> programState.[programState.[pointer]]
         | ImmediateMode -> programState.[pointer]
 
+// get the storage location
+// This is actually always PositionMode :)
 let getStorageLocation (mode: ParameterMode) (programState: int []) (locationValue: int) = 
         match mode with
         | PositionMode -> programState.[locationValue]
         | ImmediateMode -> locationValue
 
 
-// Lets execute the program recursively with a moving pointer
-let rec runProgram (pointer: int) (programState : int []) (inputs: int[]) (output: string): string =
-    let instruction = getInstruction(programState.[pointer]) 
-    let mutable newPointer = pointer
-    let mutable newOutput = output;
-    let mutable newInputs = inputs;
-    printfn "Instruction opcode %i" instruction.Opcode
-    printfn "Instruction mode: %A" instruction.Mode1
+// recurse recurse recurse
+// Given an input and an Amplifier, return the new state of the amplifier including it's output
+// take special care of initialization with the phase, halting when there is output and final completion
+let rec runProgram (amplifier: Amplifier) (input: int): Amplifier =
+    let instruction = getInstruction(amplifier.programState.[amplifier.pointer]) 
+    let { pointer=pointer; programState=programState } = amplifier
+    printfn "Instruction code %i" instruction.Opcode
+    let newAmplifierState =
+        match instruction.Opcode with
+            | 1 ->  
+                let destination = getStorageLocation instruction.Mode3 programState (pointer + 3)
+                programState.[destination] <- (getValue instruction.Mode1  programState  (pointer + 1) + getValue instruction.Mode2  programState (pointer + 2))
+                { amplifier with pointer=pointer + 4; programState=programState}
+            | 2 ->  
+                let destination = getStorageLocation instruction.Mode3 programState (pointer + 3)
+                programState.[programState.[pointer + 3]] <- (getValue instruction.Mode1  programState  (pointer + 1) * getValue instruction.Mode2  programState (pointer + 2))
+                { amplifier with pointer=pointer + 4; programState=programState}
+            | 3 ->  
+                let destination = getStorageLocation instruction.Mode1 programState (pointer + 1)
+                match amplifier.initialized with
+                    | false -> 
+                        programState.[destination] <- amplifier.phase
+                        runProgram { amplifier with pointer=pointer + 2; programState=programState; initialized=true} input
+                    | true -> 
+                        programState.[destination] <- input
+                        { amplifier with pointer=pointer + 2; programState=programState}
+                
+            | 4 ->  
+                let output = getValue instruction.Mode1  programState  (pointer + 1)
+                {amplifier with pointer=pointer + 2; output=output}        
+            | 5 ->
+                printfn "%A" instruction.Mode1
+                printfn "%A" instruction.Mode2
+                let newPointer =
+                    match getValue instruction.Mode1  programState  (pointer + 1) with
+                        | value when value <> 0 -> getValue instruction.Mode2  programState  (pointer + 2) 
+                        | _ -> pointer + 3
+                { amplifier with pointer=newPointer}    
+            | 6 ->
+                let newPointer =
+                    match getValue instruction.Mode1  programState  (pointer + 1) with
+                        | value when value = 0 -> getValue instruction.Mode2  programState  (pointer + 2) 
+                        | _ -> pointer +  3
+                { amplifier with pointer=newPointer}    
+
+            | 7 ->
+                let destination = getStorageLocation instruction.Mode3 programState (pointer + 3)
+                if getValue instruction.Mode1  programState  (pointer + 1) < getValue instruction.Mode2  programState  (pointer + 2)  
+                    then programState.[destination] <- 1 
+                    else programState.[destination] <- 0
+                { amplifier with pointer=pointer + 4; programState=programState}  
+            | 8 -> 
+                let destination = getStorageLocation instruction.Mode3 programState (pointer + 3)
+                if getValue instruction.Mode1  programState  (pointer + 1) = getValue instruction.Mode2  programState  (pointer + 2) 
+                    then programState.[destination] <- 1 
+                    else programState.[destination] <- 0   
+                { amplifier with pointer=pointer + 4; programState=programState}  
+            | 99 -> 
+                printfn "End Program"     
+                { amplifier with completed=true }
+            | _ ->  failwith("invalid instruction: " + string programState.[pointer])
+
     match instruction.Opcode with
-        | 1 ->  
-            let destination = getStorageLocation instruction.Mode3 programState (pointer + 3)
-            programState.[destination] <- (getValue instruction.Mode1  programState  (pointer + 1) + getValue instruction.Mode2  programState (pointer + 2))
-            newPointer <- pointer + 4
-        | 2 ->  
-            let destination = getStorageLocation instruction.Mode3 programState (pointer + 3)
-            programState.[programState.[pointer + 3]] <- (getValue instruction.Mode1  programState  (pointer + 1) * getValue instruction.Mode2  programState (pointer + 2))
-            newPointer <- pointer + 4
-        | 3 ->  
-            let destination = getStorageLocation instruction.Mode1 programState (pointer + 1)
-            programState.[destination] <- inputs.[0]
-            printfn "%A" inputs
-            match Array.length inputs with
-                | 1 -> newInputs <- [||]
-                | _ -> newInputs <- inputs.[1..(Array.length inputs - 1)]         
-            newPointer <- pointer + 2
-        | 4 ->  
-            // Did I forget before?
-            newOutput <- output + string (getValue instruction.Mode1  programState  (pointer + 1))
-            newPointer <- pointer + 2        
-        | 5 ->
-            if getValue instruction.Mode1  programState  (pointer + 1)  <> 0 
-                then newPointer <- getValue instruction.Mode2  programState  (pointer + 2) 
-                else newPointer <- pointer + 3
-        | 6 ->
-            if getValue instruction.Mode1  programState  (pointer + 1) = 0 
-                then newPointer <- getValue instruction.Mode2  programState  (pointer + 2) 
-                else newPointer <- pointer +  3
-        | 7 ->
-            let destination = getStorageLocation instruction.Mode3 programState (pointer + 3)
-            if getValue instruction.Mode1  programState  (pointer + 1) < getValue instruction.Mode2  programState  (pointer + 2)  
-                then programState.[destination] <- 1 
-                else programState.[destination] <- 0
-            newPointer <- pointer + 4
-        | 8 -> 
-            let destination = getStorageLocation instruction.Mode3 programState (pointer + 3)
-            if getValue instruction.Mode1  programState  (pointer + 1) = getValue instruction.Mode2  programState  (pointer + 2) 
-                then programState.[destination] <- 1 
-                else programState.[destination] <- 0   
-            newPointer <- pointer + 4
-        | 99 -> printfn "End Program"     
-        | _ ->  failwith(string programState.[pointer])
-    
-    if (instruction.Opcode = 99) then newOutput else runProgram newPointer programState newInputs newOutput
+        | 99 -> {newAmplifierState with completed=true}
+        | 4  -> 
+            printfn "Reached opcode 4"
+            newAmplifierState
+        | _  -> runProgram newAmplifierState input
    
-
-
 
 // Diagnostics test program based on day 5
 
-
-let runTest (testCode: int []) (input: int []) (expectedOutput: string) = 
-    let resultOutput = runProgram 0 testCode input ""                      
-    if (resultOutput <> expectedOutput) then failwith "Test failed" else printfn "Test succeeded"
+/// Single amplifier test
+let runTest (amplifier: Amplifier) (input: int) (expectedOutput: int) = 
+    let { output=output }= runProgram amplifier input                      
+    if ( output <> expectedOutput) then failwith "Test failed" else printfn "Test succeeded"
 
 
 let intCodeDay5Test1 = [|3;9;8;9;10;9;4;9;99;-1;8|]
-runTest intCodeDay5Test1 [|8|] "1"            
-runTest intCodeDay5Test1 [|7|] "0"    
-runTest intCodeDay5Test1 [|9|] "0"      
+let testAmplifier: Amplifier = { programState=intCodeDay5Test1; pointer=0; initialized=true; phase=0; completed=false; output=0}
+runTest testAmplifier 8 1            
+runTest testAmplifier 7 0    
+runTest testAmplifier 9 0      
 
 let intCodeDay5Test5: int [] = File.ReadAllText("./Day5_input_test5.txt").Split ','
                                     |> Array.map int
-runTest intCodeDay5Test5 [|4|] "999"
-runTest intCodeDay5Test5 [|8|] "1000"
-runTest intCodeDay5Test5 [|10|] "1001"
+let testAmplifier5: Amplifier = { testAmplifier with programState=intCodeDay5Test5}
+runTest testAmplifier5 4 999
+runTest testAmplifier5 8 1000
+runTest testAmplifier5 10 1001
                     
 let intCodeDay5: int [] = File.ReadAllText("./Day5_input.txt").Split ','
                         |> Array.map int
 
-runTest intCodeDay5 [|5|] "12111395"                  
-
-let amplifyInput (program: int []) (phase: int) (input: int): int =
-    // initialize amplifier
-    let instructionPointer:int = 0;
-    let output = runProgram instructionPointer program [|phase;input|] ""
-    int output
+let testAmplifier6 = { testAmplifier with programState=intCodeDay5}
+runTest testAmplifier6 5 12111395                  
 
 
+// Day 7 part one
 let intCode: int [] = File.ReadAllText("./Day7_input_sample1.txt").Split ','
                         |> Array.map int
-let phases: int [] = [|4;3;2;1;0|]
-let initialInput = 0
+let phases: int list = [4;3;2;1;0]
+  
+let runThrusterTest1 (program: int[]) (phases: int list) (expectedOutput: int) = 
+    let output: int = phases
+                     |> List.map (fun phase -> {phase=phase;programState=program;initialized=false;pointer=0;completed=false;output=0})
+                     |> List.fold (fun input amplifier -> 
+                                        let {output=output} = runProgram amplifier input
+                                        output 
+                                  ) 0
+                    
+    match output with 
+        | value when expectedOutput = value ->  printfn "Test succeeded with output: %i" output
+        | _ -> printf "Test failed, got %i" output
 
-phases
-    |> Array.fold (fun input phase -> amplifyInput intCode phase input ) initialInput
-    |> printfn "hurray %A"
+runThrusterTest1 intCode phases 43210
+
+
+
+let intCode2: int [] = File.ReadAllText("./Day7_input.txt").Split ','
+                        |> Array.map int
+let calculateThrusterInput (program: int[]) (phases: int list) =
+    phases
+     |> List.map (fun phase -> {phase=phase;programState=program;initialized=false;pointer=0;completed=false;output=0})
+     |> List.fold (fun input amplifier -> 
+                        let {output=output} = runProgram amplifier input
+                        output 
+                  ) 0            
+let permutations = permute [0;1;2;3;4]
+// Day 7: final calculation and verification
+let day7output = permutations
+                    |> List.map (fun phase -> calculateThrusterInput intCode2 phase)
+                    |> List.max
+match day7output with 
+    | 212460 ->  printfn "Test succeeded with output: %i" day7output
+    | _ -> printf "Test failed"
+
+
+
+// Day7: part 2
+
+// This loop has to iterate over all amplifiers
+// - first time, the phase is the initial parameter
+// - when we get the output we feed  it into the next one
+// - the final one feeds back to the first one
+// - the moment the software halts, we use the last output
+let createInitialAmplifiers (phases: int list) (program: int []): Amplifier list =
+    let baseAmplifier = { phase=phase;programState=[];initialized=false;pointer=0;completed=false;output=0}
+    phases
+        |> List.map (fun phase -> {baseAmplifier with programtState=program}
+            
+
+type LoopState = {amplifiers: Amplifier list; output:int; completed:bool}
+
+let rec loopAmplifiers (_amplifiers: Amplifier list) (input: int): int =
+    printfn "START A NEW LOOP"
+    let previousState: LoopState = {amplifiers=[];output=input;completed=false}
+    let lastLoop= _amplifiers
+                        |> List.fold (fun previousState nextAmplifier -> 
+                                printfn "RUN AMPLIFIER"
+                                printfn "%A" nextAmplifier
+                                printfn "%i" previousState.output
+                                let amplifierState = runProgram nextAmplifier previousState.output
+                                printfn "OUTPUT %A" amplifierState                          
+                                {amplifiers=List.append previousState.amplifiers [amplifierState];output=amplifierState.output;completed=amplifierState.completed}                          
+                            ) previousState
+    
+    match lastLoop with
+        | {completed=false} ->  
+            printfn "END OF NORMAL LOOP"
+            printfn "%i" lastLoop.output
+            loopAmplifiers lastLoop.amplifiers lastLoop.output
+        | {completed=true} -> 
+            printfn "DONE DONE DONE"
+            printfn "%i" lastLoop.output
+            lastLoop.output
+
+// Test 1
+let testPhase = [9;8;7;6;5]
+let intCodeTest2: int [] = File.ReadAllText("./Day7_input_sample2.txt").Split ','
+                        |> Array.map int    
+
+
+let test = createInitialAmplifiers testPhase intCodeTest2
+let testResult2 = loopAmplifiers (createInitialAmplifiers testPhase intCodeTest2) 0
+
+let permutationsPart2 = permute [5..9]
+
+permutationsPart2
+    |> List.map (fun phase -> loopAmplifiers (createInitialAmplifiers  phase intCode2) 0)
+    |> List.max
